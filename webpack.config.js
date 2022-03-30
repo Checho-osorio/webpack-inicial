b1a7ca5471ec19887a48780be0d1dd7e6c2fd5dc
Sergio Osorio <sergioa@titamedia.com>
2022-03-30 14:47:28 -0500

    ajuste webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,38 @@
 const HtmlWebPackPlugin       = require('html-webpack-plugin'); 
 const MiniCssExtractPlugin    = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MinifyPlugin            = require('babel-minify-webpack-plugin');
+const { CleanWebpackPlugin }  = require('clean-webpack-plugin');
+const TerserPlugin            = require('terser-webpack-plugin');
+const  autoprefixer           = require('autoprefixer');
+
+var   OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    
     optimization: {
-        minimizer: [ new OptimizeCssAssetsPlugin() ]
+        minimize: true,
+        minimizer: [
+          new TerserPlugin(),
+          new OptimizeCssAssetsPlugin({
+            cssProcessorPluginOptions: {
+              preset: ['default', { discardComments: { removeAll: true } }],
+            }
+          })
+        ],
+        
+    },
+    output: {
+        filename: 'js/[name].js'
     },
     module: {
         rules: [
+            { 
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                use: [
+                    'babel-loader'
+                ]
+            },
             {
                 test: /\.css$/,
                 exclude: /styles\.css$/,
@@ -18,11 +42,38 @@ module.exports = {
                 ]
             },
             {
-                test: /styles\.css$/,
+                
                 use: [
                     MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
+                    
+                    {
+                        loader: 'css-loader',
+                        options: {
+                          sourceMap: true,
+                        }
+                      },
+                      {
+                        loader: 'postcss-loader',
+                        options: {
+                          autoprefixer: {
+                            browser: ['last 2 versions']
+                          },
+                          sourceMap: true,
+                          plugins: () => [autoprefixer]
+                        }
+                      },
+                      'resolve-url-loader', // requiere sourcemap en sass
+                      {
+                        loader: 'sass-loader',
+                        options: {
+                          outputStyle: 'compressed',
+                          sourceMap: true
+                        },
+                      }
+                ],
+
+                test: /\.(css|scss)$/
+
             },
             {
                 test: /\.html$/,
@@ -34,7 +85,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(png|svg|jpg|gif)$/,
+                test: /\.(jpe?g|png|gif|webp)$/i,
                 use: [
                     {
                         loader: 'file-loader',
@@ -44,7 +95,15 @@ module.exports = {
                         }
                     }
                 ]
-            }
+              },
+              {
+                test: /\.(ttf|eot|woff2?|mp4|mp3|txt|xml|pdf)$/i,
+                use: 'file-loader?name=assets/[name].[ext]'
+              },
+              {
+                test: /\.(svg)/,
+                use: 'base64-inline-loader?limit=1000&name=[name].[ext]'
+              }
         ]
     },
     plugins: [
@@ -53,9 +112,11 @@ module.exports = {
             filename: './index.html'
         }),
         new MiniCssExtractPlugin({
-            filename: '[name].css',
+            filename: 'css/[name].css',
             ignoreOrder: false
-        })
+        }),
+        new MinifyPlugin(),
+        new CleanWebpackPlugin(),
     ]
 
 }
